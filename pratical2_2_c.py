8e9e4489b3b019cd4759907bcfd478f784195664
20CE094 <97758311+20CE094@users.noreply.github.com>
2022-01-15 00:44:13 +0530

    Add files via upload

---
+++ pratical2_2_c.py
@@ -0,0 +1,10 @@
+#Name-Nehal Patel
+#ID- 20CE094
+#Github link: https://github.com/20CE094/Practical-2-2.c.git
+#Aim: Write a Python program to add an item in a tuple.
+t=(1,2,3,4)
+v=list(t)
+#adding element to the tuple
+v.append(5)
+t=tuple(v)
+print(t)
